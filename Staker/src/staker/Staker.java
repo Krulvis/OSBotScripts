9b29873cf102c29deae72fe3ecd501196f76ab54
Krulvis <joepkleint@gmail.com>
2017-06-01 04:00:39 +0200

    Time for Staker version change

--- Staker/src/staker/Staker.java
+++ Staker/src/staker/Staker.java
@@ -28,7 +28,7 @@ import java.util.LinkedList;
  * Created by Krulvis on 29-May-17.
  */
 
-@ScriptManifest(author = "Krulvis", version = 1.01D, logo = "", info = "", name = "Staker")
+@ScriptManifest(author = "Krulvis", version = 1.02D, logo = "", info = "", name = "Staker")
 public class Staker extends ATScript {
 
     public boolean debug = false;
