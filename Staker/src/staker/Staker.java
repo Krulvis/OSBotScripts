597813e76ca90859b7971663989e9dd2b63ec06d
Tony <anthony.vanc@gmail.com>
2017-06-03 12:25:04 +0200

    change version

--- Staker/src/staker/Staker.java
+++ Staker/src/staker/Staker.java
@@ -25,7 +25,7 @@ import java.util.LinkedList;
  * Created by Krulvis on 29-May-17.
  */
 
-@ScriptManifest(author = "Krulvis", version = 1.09D, logo = "", info = "", name = "Staker")
+@ScriptManifest(author = "Krulvis", version = 1.10D, logo = "", info = "", name = "Staker")
 public class Staker extends ATScript {
 
     public boolean debug = false;
