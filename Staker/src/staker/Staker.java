e6039de43eb9718999c78ea6cd142c8bff775bf2
Krulvis <joepkleint@gmail.com>
2017-06-04 16:12:04 +0200

    Guess it's 1.12 now then

--- Staker/src/staker/Staker.java
+++ Staker/src/staker/Staker.java
@@ -25,7 +25,7 @@ import java.util.LinkedList;
  * Created by Krulvis on 29-May-17.
  */
 
-@ScriptManifest(author = "Krulvis", version = 1.11D, logo = "", info = "", name = "Staker")
+@ScriptManifest(author = "Krulvis", version = 1.12D, logo = "", info = "", name = "Staker")
 public class Staker extends ATScript {
 
     public boolean debug = false;
